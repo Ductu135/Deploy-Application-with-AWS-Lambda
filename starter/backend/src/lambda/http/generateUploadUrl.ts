780b96e69095bd465c9d7e97194fb084edfe0437
Tu DucBui <ductuhp135@gmail.com>
2024-10-19 00:02:25 +0700

    update client

--- starter/backend/src/lambda/http/generateUploadUrl.ts
+++ starter/backend/src/lambda/http/generateUploadUrl.ts
@@ -14,6 +14,9 @@ export const handler =  middy(
 
   return {
     statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       url: url
     })
